8c2ca06279e003382026190adb2aad4420c115ed
Ataix <atai_ks@yahoo.com>
2021-07-01 17:33:38 +0600

    Deleted filters.py
    * Deleted filters.py

--- apps/order/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-
-from apps.order.models import Order
-
-
-class OrderFilter(django_filters.FilterSet):
-    class Meta:
-        model = Order
-        fields = ('customer_phone',)
